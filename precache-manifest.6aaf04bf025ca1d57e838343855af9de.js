a3640c5c86db1ece444d55a652072dc3a79eb1a5
jojomak13 <jojomak350@gmail.com>
2019-08-29 12:40:50 +0200

    Updates

---
+++ precache-manifest.6aaf04bf025ca1d57e838343855af9de.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c4ab7997d6c65ce364d6115ac0f649a",
+    "url": "/pomodoro-clock/index.html"
+  },
+  {
+    "revision": "de36524426812d2c86d6",
+    "url": "/pomodoro-clock/static/css/main.e432cab8.chunk.css"
+  },
+  {
+    "revision": "3d1c99499a49abb3ffaf",
+    "url": "/pomodoro-clock/static/js/2.938b5dfe.chunk.js"
+  },
+  {
+    "revision": "de36524426812d2c86d6",
+    "url": "/pomodoro-clock/static/js/main.3f46f627.chunk.js"
+  },
+  {
+    "revision": "d9270eff08a7abdf7269",
+    "url": "/pomodoro-clock/static/js/runtime~main.36405bfe.js"
+  }
+]);
